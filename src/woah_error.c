812a4589c1fcb08132a7c400353367b6a35727dc
Conrad Khakhria <conradjkhakhria@gmail.com>
2020-10-30 16:50:07 +0000

    Better syntax errors

--- src/woah_error.c
+++ src/woah_error.c
@@ -12,21 +12,30 @@ void woah_syntax_error(int line_no, int col_no)
     }
 
     int margin_indent = NUMLEN(line_no) + 3;
+    int line_indent   = 0;
 
     // Line 1 of error message
     print_indent(margin_indent);
     fprintf(stderr, "|\n");
+
     // Line 2
-    fprintf(stderr, " %d |", line_no);
-    while (content_buffer[i] != '\n') {
+    fprintf(stderr, " %d | ", line_no);
+
+    while (content_buffer[i] == ' ') {
+        i++;
+        line_indent++;
+    }
+
+    while (content_buffer[i] != '\n' && content_buffer[i] != '\x00') {
         fprintf(stderr, "%c", content_buffer[i]);
         i++;
     }
     fprintf(stderr, "\n");
+
     // Line 3
     print_indent(margin_indent);
     fprintf(stderr, "|");
-    print_indent(col_no);
+    print_indent(col_no - line_indent);
     fprintf(stderr, "^\n");
 
     exit(1);
--- src/woah_error.h
+++ src/woah_error.h
@@ -6,10 +6,15 @@
 
 #define NUMLEN(x) (int)log10((float)x)
 #define WSEL1(...) do {                                                 \
-    fprintf(stderr, "Woah: Syntax Error in file '%s'\n=> ", filename);    \
+    fprintf(stderr, "Woah: Syntax Error in file '%s'\n=> ", filename);  \
     fprintf(stderr, __VA_ARGS__);                                       \
 } while (0)
-
+#define MALLOCERR(_BUF, _CODE) do {                                         \
+    if (_BUF == NULL) {                                                     \
+        WSEL1("internal malloc() error (sorry): error code 0x%x\n", _CODE); \
+        exit(1);                                                            \
+    }                                                                       \
+} while (0)
 
 void print_indent(int indent_size);
 
