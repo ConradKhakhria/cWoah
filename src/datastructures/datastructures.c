2f7709e6196b74b9fc101fd803f5f3e24ec49f6c
Conrad Khakhria <conradjkhakhria@gmail.com>
2020-11-04 16:27:30 +0000

    used HANDLEMALLOCERR() macro

--- src/datastructures/datastructures.c
+++ src/datastructures/datastructures.c
@@ -5,13 +5,13 @@ Array make_array(size_t buffer_size)
 {
     Array retarray = calloc(1, sizeof(struct Array));
 
-    MALLOCERR(retarray, 2);
+    HANDLEMALLOCERR(retarray, 2);
 
     retarray->buffer     = calloc(ARRAY_BUFFER_SIZE, sizeof(void *));
     retarray->buffer_len = ARRAY_BUFFER_SIZE;
     retarray->index      = 0;
 
-    MALLOCERR(retarray->buffer, 3);
+    HANDLEMALLOCERR(retarray->buffer, 3);
 
     return retarray;
 }
@@ -21,7 +21,7 @@ void array_add(Array array,  void *val)
     if (array->index >= array->buffer_len) {
         void **new_buffer = realloc(array->buffer, array->buffer_len + ARRAY_BUFFER_GROW);
 
-        MALLOCERR(new_buffer, 4);
+        HANDLEMALLOCERR(new_buffer, 4);
 
         free(array->buffer);
         array->buffer = new_buffer;
@@ -33,8 +33,13 @@ void array_add(Array array,  void *val)
 void *array_index(Array array, int index)
 {
     if (index < 0 || array->index < index) {
-        fprintf(stderr, "Woah: index %d out of range [0, %d]\n", index, array->index);
-        exit(INDEX_ERROR);
+        WSEL1(
+            "internal error 0x%x - attempt to get %dth index of list length %d\n",
+            INDEX_OUT_OF_BOUNDS,
+            index,
+            array->index
+        );
+        exit(INDEX_OUT_OF_BOUNDS);
     } else {
         return array->buffer[index];
     }
