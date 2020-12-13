ff0489600f4568393b0f3b7d2417f2b18a1cced0
Conrad Khakhria <conradjkhakhria@gmail.com>
2020-12-13 00:44:59 +0000

    added parse_type.h import

--- src/parse/parse.h
+++ src/parse/parse.h
@@ -2,7 +2,10 @@
 #pragma once
 
 #include "../datastructures/datastructures.h"
+#include "./parse_type.h"
 
 int traverse_block(Array prog, int start, int end, int open, int close);
 
-int collect_blocks(Array tokens, Array* blocks);
+struct WFunction* collect_block_function(Array tokens_array, int index);
+
+int collect_blocks(Array tokens_array, Array* blocks);
