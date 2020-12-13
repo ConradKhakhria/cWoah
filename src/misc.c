78d63ce0453a5fc3db9f66e7719690603857cc83
Conrad Khakhria <conradjkhakhria@gmail.com>
2020-12-13 19:08:38 +0000

    Created misc.X files for miscellaneous utilities

--- src/misc.c
+++ src/misc.c
@@ -14,8 +14,56 @@ void print_slice(char* array, int start, int end)
     *
     * Modifies nothing.
     */
-
     for (int i = start; i < end; i++) {
         printf("%c", array[i]);
+    }
+}
+
+void print_type(struct WType* type)
+{
+    /* Displays a type annotation. */
+    struct WType** derivs     = type->derivs;
+    struct Token* struct_name = type->derivs;
+    int i;
+
+    switch (type->type_form) {
+        case TF_LIST:
+            print_type(type->derivs);
+            printf("[]");
+            break;
+
+        case TF_POINTER:
+            printf("&");
+            print_type(type->derivs);
+
+        case TF_PARAMETRIC:
+            print_slice(
+                program_source_buffer, type->name->start_i, type->name->end_i
+            );
+
+            printf("<");
+
+            for (i = 0; i < type->num; i++) {
+                print_type(derivs[i]);
+                printf(", ");
+            }
+
+            printf(">");
+            break;
+
+        case TF_STRUCT:
+            printf("struct ");
+            print_slice(
+                program_source_buffer, struct_name->start_i, struct_name->end_i
+            );
+            break;
+
+        case TF_ATOMIC:
+            printf("[LIST OF TYPES NOT YET IMPLEMENTED]");
+            break;
+
+        default: // This should never happen
+            error_message("Unexpected internal error in print_type()\n");
+            exit(-1);
     }
 }
--- src/misc.h
+++ src/misc.h
@@ -3,4 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "./parse/parse_type.h"
+
 void print_slice(char* array, int start, int end);
+
+void print_type(struct WType* type);
