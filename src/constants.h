70530deb5ce983f8b5ad4469450e030f31224b95
Conrad Khakhria <conradjkhakhria@gmail.com>
2020-12-17 14:37:52 +0000

    Added INBUILT_TYPES_COUNT

--- src/constants.h
+++ src/constants.h
@@ -1,9 +1,10 @@
 #pragma once
 
 // Various size parameters
-#define ARRAY_BUFFER_SIZE 0x1000
-#define ARRAY_BUFFER_GROW 0x200
-#define TOKENS_ARRAY_SIZE 0x1000
+#define ARRAY_BUFFER_SIZE   0x1000
+#define ARRAY_BUFFER_GROW   0x200
+#define TOKENS_ARRAY_SIZE   0x1000
+#define INBUILT_TYPES_COUNT 19
 
 // These are here so that they can be used globally throughout the program
 // (read-only except for in /src/main.c)
