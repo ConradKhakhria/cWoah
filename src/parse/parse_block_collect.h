c58396584e73232edd3b08a597e7cdbd0eff340d
Conrad Khakhria <conradjkhakhria@gmail.com>
2020-12-18 18:35:40 +0000

    Added declaration for collect_block_typedef() and collect_block_globals()

--- src/parse/parse_block_collect.h
+++ src/parse/parse_block_collect.h
@@ -7,12 +7,19 @@
 #pragma once
 
 #include "../datastructures/datastructures.h"
+#include "../misc.h"
 #include "./parse_type.h"
 
 int traverse_block(Array prog, int start, int end, int open, int close);
 
-struct WFunction* collect_block_function(Array tokens_array, int index);
+struct WFunction* collect_block_function(Array tokens_array, int* index);
 
-struct WStruct* collect_block_struct(Array tokens_array, int index);
+struct WStruct* collect_block_struct(Array tokens_array, int* index);
+
+struct WTypedef* collect_block_typedef(Array tokens_array, int* index);
+
+struct WModuleImport* collect_block_import(Array tokens_array, int* index);
+
+struct WGlobals* collect_block_globals(Array tokens_array, int* index);
 
 int collect_blocks(Array tokens_array, Array* blocks);
