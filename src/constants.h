c37aff7cc2e98f47253e7338ce3bd516b975f79f
Conrad Khakhria <conradjkhakhria@gmail.com>
2020-12-23 12:25:22 +0000

    changed inbuilt_types to keywords and added a KEYWORD_COUNT constant

--- src/constants.h
+++ src/constants.h
@@ -5,9 +5,10 @@
 #define ARRAY_BUFFER_GROW   0x200
 #define TOKENS_ARRAY_SIZE   0x1000
 #define INBUILT_TYPES_COUNT 19
+#define KEYWORD_COUNT       43
 
 // These are here so that they can be used globally throughout the program
 // (read-only except for in /src/main.c)
 char* program_source_buffer;
 char* filename;
-char* inbuilt_types[19];
+char* keywords[43];
