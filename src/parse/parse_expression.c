6c396d1d7107ec2b1447e52ad3650ddd50f3fbc0
Conrad Khakhria <conradjkhakhria@gmail.com>
2021-01-07 17:00:20 +0000

    Implemented all compound expression functions as well as parse_macro_use

--- src/parse/parse_expression.c
+++ src/parse/parse_expression.c
@@ -1,31 +1,15 @@
 #include "parse_expression.h"
 
-bool compound_boolean_expression(Array tokens_array, struct BoolExpr* expr, int start, int end)
+bool compound_expression(Array tokens_array, int* operators, int operator_count,
+                         struct ParseExpr* expr, int start, int end)
 {
-   /* Determines whether or not the expression is a compound expression.
-    *
-    * Parameters
-    * ----------
-    * - same as parse_boolean_expression() and also:
-    * 
-    * - struct BoolExpr expr: the return value for parse_boolean_expression().
-    * 
-    * Modifies
-    * --------
-    * - struct BoolExpr* expr if and only if it is compound.
-    * 
-    * Returns: whether or not it is compound.
-    * -------
-    */
+   /* Main logic for compound_boolean_expression() and compound_math_expression() */
     struct Token** tokens = (struct Token **)tokens_array->buffer;
-    int operators[10]     = { T_OR, T_AND, T_XOR, T_NOT, T_DBL_EQUALS,
-                              T_INEQ, T_LT, T_GT, T_LEQ, T_GEQ };
 
-    // Round brackets and square brackets
+    // To ignore sub-expressions in round and square brackets
     int bracket_depths[2] = { 0, 0 };
 
-    // If it's a compound expression (e.g. "var1 and var2 or not var3")
-    for (int op = 0; op < 9; op++) {
+    for (int op = 0; op < operator_count - 1; op++) {
         for (int i = start; i <= end; i++) {
             if (tokens[i]->token_type == T_OPEN_BRKT) {
                 bracket_depths[RND] += 1;
@@ -36,7 +20,7 @@ bool compound_boolean_expression(Array tokens_array, struct BoolExpr* expr, int 
             } else if (tokens[i]->token_type == T_CLOSE_SQ_BRKT) {
                 bracket_depths[SQU] -= 1;
             } else if (tokens[i]->token_type == operators[op]) {
-                expr->type  = BET_NESTED;
+                expr->type  = (operators[0] == T_OR) ? PET_COMPOUND_BOOLEAN : PET_COMPOUND_ARITHMETIC;
                 expr->value = operators[op];
                 expr->expression.derivs = malloc(sizeof(struct BoolExpr *) * 2);
 
@@ -68,7 +52,77 @@ bool compound_boolean_expression(Array tokens_array, struct BoolExpr* expr, int 
     return false;
 }
 
-struct FunctionCall* parse_function_call(Array tokens_array, struct BoolExpr* expr, int start, int end)
+bool compound_boolean_expression(Array tokens_array, struct ParseExpr* expr, int start, int end)
+{
+   /* Determines whether or not the expression is a compound boolean expression.
+    *
+    * Parameters
+    * ----------
+    * - Array tokens_array: the tokens containing the boolean expression.
+    * 
+    * - struct ParseExpr expr: the expression to be modified if it is a compound expression.
+    * 
+    * - int start: the index of the first token in the expression.
+    * 
+    * - int end: the index of the last token in the expression.
+    * 
+    * Modifies
+    * --------
+    * - struct BoolExpr* expr if and only if it is compound.
+    * 
+    * Returns: whether or not it is compound.
+    * -------
+    */
+
+    int operators[10] = {
+        T_OR,
+        T_AND,
+        T_XOR,
+        T_NOT,
+        T_DBL_EQUALS,
+        T_LT,
+        T_GT,
+        T_LEQ,
+        T_GEQ
+    };
+
+    return compound_expression(tokens_array, operators, 10, expr, start, end);
+}
+
+bool compound_math_expression(Array tokens_array, struct ParseExpr* expr, int start, int end)
+{
+   /* Determines whether or not the expression is a compound maths expression.
+    *
+    * Parameters
+    * ----------
+    * - Array tokens_array: the tokens containing the math expression.
+    * 
+    * - struct ParseExpr expr: the expression to be modified if it is a compound maths expression.
+    * 
+    * - int start: the index of the first token in the expression.
+    * 
+    * - int end: the index of the last token in the expression.
+    * 
+    * Modifies
+    * --------
+    * - struct ParseExpr* expr if and only if it is a compound maths expression.
+    * 
+    * Returns: whether or not it is a compound expression.
+    * -------
+    */
+
+    int operators[5] = {
+        T_PLUS,
+        T_MINUS,
+        T_ASTERISK,
+        T_FWD_SLASH,
+        T_PERCENT
+    };
+
+    return compound_expression(tokens_array, operators, 5, expr, start, end);
+}
+
+bool parse_function_call(Array tokens_array, struct ParseExpr* expr, int start, int end)
 {
    /* Parses a function call into a struct FunctionCall*
     *
@@ -86,7 +140,7 @@ struct FunctionCall* parse_function_call(Array tokens_array, struct BoolExpr* ex
     * 
     * Returns
     * -------
-    * A pointer to a nested struct FunctionCall containing the expression.
+    * Whether it is in fact a function call.
     * 
     */
     struct Token** tokens     = (struct Token **)tokens_array->buffer;
@@ -121,48 +175,73 @@ struct FunctionCall* parse_function_call(Array tokens_array, struct BoolExpr* ex
     while (true) {
         
     }
-    
-   
+
+
 }
 
-struct BoolExpr* parse_boolean_expression(Array tokens_array, int start, int end)
+bool parse_macro_use(Array tokens_array, struct ParseExpr* expr, int start, int end)
 {
-   /* Parses a boolean expression into a struct BoolExpr*
-    *
+   /* Checks if the expression is a usage of a built-in macro
+    * 
     * Parameters
     * ----------
-    * - Array tokens_array: the tokens containing the boolean expression.
+    * ""        ""          ""
     * 
-    * - int start: the first token in the expression.
-    * 
-    * - int end: the last token in the expression.
-    * 
-    * Modifies: Nothing.
+    * Modifies
     * --------
+    * ""        ""          ""
     * 
     * Returns
     * -------
-    * A pointer to a nested struct BoolExpr containing the expression.
+    * Whether or not the expression is a macro use
     */
+
     struct Token** tokens = (struct Token **)tokens_array->buffer;
-    struct BoolExpr* expr = malloc(sizeof(struct BoolExpr));
 
-    malloc_error(expr, PARSE_BOOLEAN_EXPRESSION_ALLOC_BOOLEXPR);
+    expr->type = PET_MACRO_USE;
 
-    if (start == end && (tokens[start]->token_type == T_NAME
-    || tokens[start]->token_type == T_TRUE || tokens[start]->token_type == T_FALSE)) {
-        expr->type  = BET_ATOM;
-        expr->value = tokens[start]->token_type;
+    switch (tokens[start]->token_type) {
+        case T_CAST:
+            if (tokens[start + 1]->token_type != T_OPEN_SQ_BRKT
+            ||  tokens[end]->token_type != T_CLOSE_SQ_BRKT) {
+                error_message("cast! macro not followed by expression in square brackets.\n");
+                error_println(tokens[start]->line_no, tokens[start]->col_no);
+                exit(-SYNTAX_ERROR);
+            } else {
+                expr->expression.macro_use = (struct MacroUse) {
+                    .macro_name = T_CAST,
+                    .derivs     = parse_general_expression(
+                        tokens_array, start + 2, end - 1
+                    )
+                };
+            }
 
-        return expr;
-    } else if (compound_boolean_expression(tokens_array, expr, start, end)) {
-        return expr;
-    } else if ()
+            return true;
 
-    else {
-        error_message("Unrecognised syntax in boolean expression.\n");
-        error_println(tokens[start]->line_no, tokens[start]->col_no);
-        exit(-SYNTAX_ERROR);
+        case T_HEAP:
+        case T_STACK:
+            if (tokens[start + 1]->token_type != T_OPEN_SQ_BRKT
+            ||  tokens[end]->token_type != T_CLOSE_SQ_BRKT) {
+                error_message("macro use not followed by square brackets.\n");
+                error_println(tokens[start]->line_no, tokens[start]->col_no);
+                exit(-SYNTAX_ERROR);
+            } else if (start + 2 == end) {
+                expr->expression.macro_use = (struct MacroUse) {
+                    .macro_name = tokens[start]->token_type,
+                    .derivs     = NULL
+                };
+            } else {
+                expr->expression.macro_use = (struct MacroUse) {
+                    .macro_name = T_HEAP,
+                    .derivs     = parse_general_expression(
+                        tokens_array, start + 2, end - 1
+                    )
+                };
+            }
+
+            return true;
+    
+        default:
+            return false;
     }
 }
-
